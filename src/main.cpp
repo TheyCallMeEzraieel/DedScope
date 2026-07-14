fc3550a8626e62cf3e5fd2aac2eac85f2af975ca
Silhouette <80192196+TheyCallMeEzraieel@users.noreply.github.com>
2026-07-14 20:12:53 +0330

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include "Utils/Logger.h"
 #include "Utils/Console.h"
+#include "Process/ProcessManager.h"
 
 int main()
 {
-    Console::Write("Welcome!");
+    Console::Write("Welcome!\n");
     return 0;
 }
